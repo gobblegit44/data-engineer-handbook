395182703b01d681f8fb1b2917301963e34d39ee
gobblegit44 <spurthipriya@gmail.com>
2025-01-22 13:56:11 -0500

    Update HW_factdatamodeling.sql

--- bootcamp/materials/2-fact-data-modeling/homework/HW_factdatamodeling.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/HW_factdatamodeling.sql
@@ -14,13 +14,48 @@ SELECT * FROM
     - or you could have `browser_type` as a column with multiple rows for each user (either way works, just be consistent!)
 
 CREATE TABLE user_devices_cumulated (
-	user_id INTEGER,
+	user_id DECIMAL,
 	browser_type TEXT,
-	device_activity_datelist ARRAY[DATE],
+	device_activity_datelist DATE[],
 	PRIMARY KEY (user_id,browser_type)
 )
 
-- A cumulative query to generate `device_activity_datelist` from `events`
+- A cumulative query to generate `device_activity_datelist` derived from `events`
+
+WITH yesterday AS (
+    SELECT * FROM user_devices_cumulated
+),
+today AS (
+	SELECT e.user_id,
+			d.browser_type,
+			ARRAY_AGG(DISTINCT DATE(e.event_time) ORDER BY DATE(e.event_time)) as device_activity_datelist
+			
+	FROM events e
+	JOIN (SELECT DISTINCT device_id, browser_type FROM devices) d
+	ON e.device_id = d.device_id 
+	WHERE e.user_id IS NOT NULL
+	GROUP BY e.user_id, d.browser_type
+	ORDER BY e.user_id, d.browser_type
+)
+
+INSERT INTO user_devices_cumulated
+SELECT
+    COALESCE(t.user_id, y.user_id) as user_id,
+    COALESCE(t.browser_type, y.browser_type) as browser_type,
+    CASE 
+        WHEN y.device_activity_datelist IS NULL THEN t.device_activity_datelist
+        WHEN t.device_activity_datelist IS NOT NULL THEN 
+            (SELECT ARRAY_AGG(DISTINCT dates ORDER BY dates)
+             FROM (
+                 SELECT UNNEST(y.device_activity_datelist || t.device_activity_datelist) as dates
+             ) unique_dates)
+        ELSE y.device_activity_datelist
+    END as device_activity_datelist
+FROM today t
+FULL OUTER JOIN yesterday y
+    ON t.user_id = y.user_id 
+    AND t.browser_type = y.browser_type
+
 
 - A `datelist_int` generation query. Convert the `device_activity_datelist` column into a `datelist_int` column 
 
